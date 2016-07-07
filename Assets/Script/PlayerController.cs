f299d05d78436bc6c0fdd68fdd877e33ac0ee607
JoaoSant0s <joao@manifestohut.com.br>
2016-07-06 21:28:08 -0300

    Firts Commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	float speed = 5f;
+
+	[SerializeField] 
+	float jumpForce = 350f;
+	[SerializeField] 
+	float groundedRadius = .2f;
+
+	[SerializeField]
+	LayerMask whatIsGround;
+	
+	Vector3 velocity;
+
+	Rigidbody rigidbody;
+	bool grounded = false;
+	Transform groundCheck;								// A position marking where to check if the player is grounded.
+
+
+
+	void Awake () {
+		groundCheck = transform.Find("GroundCheck");
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate(){
+		grounded = Physics.OverlapSphere(groundCheck.position, groundedRadius, whatIsGround).Length > 0;
+		rigidbody.position += velocity * Time.deltaTime;
+	}
+	
+	public void Move(Vector2 direction, bool jump ){
+		velocity = direction * speed;
+
+		if (grounded && jump) {
+			rigidbody.AddForce (new Vector2 (0f, jumpForce));
+		}
+	}
+}
